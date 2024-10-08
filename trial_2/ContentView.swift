a6f22c331b14c1cbeab5234a2b588602c3773489
pentaxiont <alper.camci@icloud.com>
2024-10-08 19:44:21 +0300

    test2

--- trial_2/ContentView.swift
+++ trial_2/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
                 }
                 ToolbarItem {
                     Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus adana")
+                        Label("Add Item", systemImage: "plus")
                     }
                 }
             }
